95e9a5a7fb5480926bff621ee532e34a753462d2
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-19 07:34:29 -0300

    Rename the entry point to bundle management.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    "entry": './app/index.js',
+    "entry": './app/app.js',
     "output": {
         path: path.join(__dirname, 'build'),
         filename: 'bundle.js'
