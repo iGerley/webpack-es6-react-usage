171210c53ff5f2d5eb39f22cb66e1b9d38e8cad5
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-20 07:44:21 -0300

    Adjusting the structure folders and imports file organizations.

--- src/app.js
+++ src/app.js
@@ -1,34 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import WeatherApp from './components/WeatherApp'; 
 
-const api_key = 'openweathermap_generated_key';
-
-class App extends Component {
-  state = {
-    city: 'Goiânia,br',
-    description: ''
-  };
-
-  componentDidMount() {
-    this.grabWeather(this.state.city);
-  }
-
-  grabWeather = (city) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
-    .then(response => response.json())
-    .then(json => { 
-      this.setState({description: json.weather[0].description})
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Weather Report for {this.state.city}</h1>
-        <h2>{this.state.description}</h2>
-      </div>
-    );
-  }  
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<WeatherApp />, document.getElementById('root'))
---
+++ src/components/WeatherApp.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const api_key = 'openweathermap_generated_key';
+
+export default class WeatherApp extends Component {
+  state = {
+    city: 'Goiânia,br',
+    description: ''
+  };
+
+  componentDidMount() {
+    this.grabWeather(this.state.city);
+  }
+
+  grabWeather = (city) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ description: json.weather[0].description })
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Weather Report for {this.state.city}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
+    );
+  }
+}
