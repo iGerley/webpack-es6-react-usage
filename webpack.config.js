dee257cefadec80ac9541981e56581b09bf317bb
iGerley <gerleyadrianomiranda@gmail.com>
2018-04-02 15:02:57 -0300

    Improved the code organization.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    "entry": './src/app.js',
-    "output": {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        port: 3000,
-        contentBase: './public',
-        inline: true
-    }
+  "entry": './src/app.js',
+  "output": {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    port: 3000,
+    contentBase: './public',
+    inline: true
+  }
 }
